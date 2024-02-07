f92e9e97f7b7d8227ed928214092b22d31497827
tamerlankartaev <tamerlankartaev04@gmail.com>
2024-02-07 21:31:14 +0600

    First commit of Project

---
+++ project/src/test/java/kz/sdu/project/AttendanceSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.sdu.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AttendanceSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
