b228c501abbd34b904948179ec197e3940ceed5d
tamerlankartaev <tamerlankartaev04@gmail.com>
2024-02-10 16:49:10 +0600

    TASK-1 add liquibase configuration and initialize entities to database

--- project/src/main/java/kz/sdu/project/config/LiquibaseConfig.java
+++ project/src/main/java/kz/sdu/project/config/LiquibaseConfig.java
@@ -1,7 +1,27 @@
 package kz.sdu.project.config;
 
+import liquibase.integration.spring.SpringLiquibase;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
 /***
  * TODO : настроить конфигурацию Liquibase
  */
+@Configuration
 public class LiquibaseConfig {
+    @Value("${spring.datasource.url}")
+    private String databaseUrl;
+
+    @Bean
+    public SpringLiquibase liquibase(DataSource dataSource) {
+        SpringLiquibase liquibase = new SpringLiquibase();
+        liquibase.setDataSource(dataSource);
+        liquibase.setChangeLog("classpath:liquibase/master.xml");
+        liquibase.setShouldRun(true); // Это флаг, чтобы Liquibase выполнялся только при запуске
+        liquibase.setDropFirst(false); // Установите true, если хотите удалять базу данных перед применением изменений
+        return liquibase;
+    }
 }
