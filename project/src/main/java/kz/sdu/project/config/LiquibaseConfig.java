3355a6e3e91e79aa33432cc9c157dc820c7a5965
tamerlankartaev <tamerlankartaev04@gmail.com>
2024-02-10 16:52:19 +0600

    TASK-1 remove todo

--- project/src/main/java/kz/sdu/project/config/LiquibaseConfig.java
+++ project/src/main/java/kz/sdu/project/config/LiquibaseConfig.java
@@ -7,9 +7,7 @@ import org.springframework.context.annotation.Configuration;
 
 import javax.sql.DataSource;
 
-/***
- * TODO : настроить конфигурацию Liquibase
- */
+
 @Configuration
 public class LiquibaseConfig {
     @Value("${spring.datasource.url}")
