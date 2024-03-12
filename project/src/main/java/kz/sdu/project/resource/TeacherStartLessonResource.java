cc4dd1dcf6e449a86f4cabf4a456c591d1e33b41
yessetassan <139701904+yessetassan@users.noreply.github.com>
2024-03-12 16:42:32 +0500

    A little Changes in DTO and AttStatusService...

---
+++ project/src/main/java/kz/sdu/project/resource/TeacherStartLessonResource.java
@@ -0,0 +1,32 @@
+package kz.sdu.project.resource;
+
+import kz.sdu.project.dto.RequestBody3DTO;
+import kz.sdu.project.service.TeacherStartLessonService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/teacherStartLesson")
+@Slf4j
+public class TeacherStartLessonResource {
+
+    private final TeacherStartLessonService teacherStartLessonService;
+
+    @Autowired
+    public TeacherStartLessonResource(TeacherStartLessonService teacherStartLessonService) {
+        this.teacherStartLessonService = teacherStartLessonService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Map<String, String>> startByManually(@RequestBody @Valid RequestBody3DTO requestBody3DTO) { {
+        log.info("Lesson Start by Teacher process... with {}", requestBody3DTO);
+        return ResponseEntity.ok().body(teacherStartLessonService.start(requestBody3DTO));
+    }
+
+    }
+}
