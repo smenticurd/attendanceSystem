9ff738d86feaadeaf22060ab5bc39f1005eb6472
yessetassan <139701904+yessetassan@users.noreply.github.com>
2024-03-25 00:14:39 +0500

    Some little Change...

--- project/src/main/resources/liquibase/changelog/20240309161500_add_teacher_all.sql
+++ project/src/main/resources/liquibase/changelog/20240309161500_add_teacher_all.sql
@@ -3,5 +3,5 @@ INSERT INTO person (first_name, last_name, middle_name, login, email) VALUES
 ('Zoe', 'Scott', 'Alice', 'zoe_scott', 'zoe_scott@example.com'),
 ('Logan', 'Adams', 'Daniel', 'logan_adams', 'logan_adams@example.com'),
 ('Lily', 'Baker', 'Jean', 'lily_baker', 'lily_baker@example.com'),
-('Ryan', 'Gonzalez', 'Keith', 'ryan_gonzalez', 'ryan_gonzalez@example.com')
+('Khamza', 'Kabylbek', 'Kanatbek', 'khamzaaa01', 'khamzaaa01@example.com')
 ON CONFLICT (login) DO NOTHING;
